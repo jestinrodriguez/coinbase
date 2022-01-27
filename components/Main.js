ef9966f9c3a0f2f3972ff49549b03a2c38ea03cd
Jestin Rodriguez <jestinrenzo.rodriguez@gmail.com>
2022-01-26 17:50:50 -0800

    implemented dashboard features

--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Portfolio from "./Portfolio";
 import styled from "styled-components";
+import Promos from "./Promos";
 
 const Main = () => {
   return (
     <Wrapper>
       <Portfolio />
+      <Promos />
     </Wrapper>
   );
 };
@@ -16,6 +18,10 @@ const Wrapper = styled.div`
   display: flex;
   max-height: calc(100vh - 64px);
   overflow: hidden;
+  overflow-y: scroll;
+  :: -webkit-scrollbar {
+    display: none;
+  }
 
   & div {
     border-radius: 0.4rem;
